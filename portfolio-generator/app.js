8dbf9adff7fa70f0628e5a8e383d23cf95cbcb46
Daylan-lewandowski <74326816+Daylan-lewandowski@users.noreply.github.com>
2021-02-01 22:26:14 -0600

    Working on adding html page

--- portfolio-generator/app.js
+++ portfolio-generator/app.js
@@ -1,11 +1,19 @@
-const printProfileData = profileDataArr => {
-    // This ...
-    for (let i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
-    
-    console.log('================');
+const profileDataArgs = process.argv.slice(2);
 
-    // is the same as this ...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};t 
+// const printProfileData = profileDataArr => {
+//   // This...
+//   for (let i = 0; i < profileDataArr.length; i += 1) {
+//     console.log(profileDataArr[i]);
+//   }
+
+//   console.log('================');
+
+//   // Is the same as this...
+//   profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
+
+// printProfileData(profileDataArgs);
+
+
+const generatePage = (username, gitHubName) => `Name : ${username}, Github: ${gitHubName}`;
+console.log(generatePage('Jane', 'janehub'));
