6c70bc468e6ee16bfa53289655b17078d29781d3
Daylan-lewandowski <74326816+Daylan-lewandowski@users.noreply.github.com>
2021-02-03 15:55:53 -0600

    updated HTML template

--- portfolio-generator/app.js
+++ portfolio-generator/app.js
@@ -1,7 +1,7 @@
 const inquirer = require('inquirer')
-// const fs = require('fs');
-// const generatePage = require('./src/page-template');
-// const pageHTML = generatePage(name, gitHub)
+const fs = require('fs');
+const generatePage = require('./src/page-template');
+
 
 const promptUser = () => {
 return inquirer.prompt([
@@ -133,7 +133,13 @@ const promptProject = portfolioData => {
 promptUser()
 .then(promptProject)
 .then(portfolioData => {
-  console.log(portfolioData);
+  const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw new Error(err);
+
+      console.log('Page created! Check out index.html in this directory to see it!');
+     });
 });
 
 // fs.writeFile('./index.html', pageHTML , err => {
