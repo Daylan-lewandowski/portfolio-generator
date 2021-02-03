fff4521ca72fdcb15783cc1c0dcf6abaf90801d5
Daylan-lewandowski <74326816+Daylan-lewandowski@users.noreply.github.com>
2021-02-03 13:15:01 -0600

    able to add multiple projects

--- portfolio-generator/app.js
+++ portfolio-generator/app.js
@@ -3,15 +3,85 @@ const inquirer = require('inquirer')
 // const generatePage = require('./src/page-template');
 // const pageHTML = generatePage(name, gitHub)
 
-inquirer
-.prompt([
+const promptUser = () => {
+return inquirer.prompt([
   {
     type: 'input',
     name: 'name',
     message: 'What is your name?'
+  },
+  {
+    type: 'input',
+    name: 'gitHub',
+    message: 'Enter your GitHub username'
+  },
+  {
+    type: 'input',
+    name: 'about',
+    message: 'Provide some info about yourself:'
   }
-])
-.then(answers => console.log(answers));
+]);
+};
+
+
+const promptProject = portfolioData => {
+  console.log (`
+  ==================
+  Add a New Project
+  ==================
+  `);
+  if (!portfolioData.projects){
+    portfolioData.projects= [];
+  }
+  return inquirer.prompt ([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is the name of your project?'
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Provide a description of the project (required)'
+    },
+    {
+      type: 'checkbox',
+      name:'languages',
+      message: 'What did you build this project with? (check all that apply)',
+      choices: ['Javascript' , 'HTML', 'CSS', 'ES6', "jQuery", 'Bootstrap', 'Node']
+    },
+    {
+      type: 'input',
+      name: 'link',
+      message: 'Enter the GitHub link to your project. (required)'
+    },
+    {
+      type: 'confirm',
+      name: 'feature',
+      message: 'Would you like to feature this project?',
+      default: false
+    },
+    {
+      type: 'confirm',
+      name: 'confirmAddProject',
+      message: 'Would you like to enter another project?',
+      default: false 
+    }
+  ])
+  .then(projectData => {
+    portfolioData.projects.push(projectData);
+    if (projectData.confirmAddProject) {
+      return promptProject(portfolioData);
+    }else {
+      return portfolioData;
+    }
+  });
+};
+promptUser()
+.then(promptProject)
+.then(portfolioData => {
+  console.log(portfolioData);
+});
 
 // fs.writeFile('./index.html', pageHTML , err => {
 //   if (err) throw err;
@@ -33,4 +103,4 @@ inquirer
 //   profileDataArr.forEach(profileItem => console.log(profileItem));
 // };
 
-// printProfileData(profileDataArgs);
+// printProfileData(profileDataArgs)}
